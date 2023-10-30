f414106547dca88cf1eaf4ada9831060c9b356e6
rajicdalibor <rajicdalibor@yahoo.com>
2023-10-30 10:33:31 +0100

    dsfbvdsfb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Layout from "./components/Layout";
-import ReactGA from "react-ga4";
 
 function App() {
-    ReactGA.initialize("G-FEVJ710W9C");
-
     return (
         <div>
           <Layout/>
